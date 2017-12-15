b3a168adbd1061b1b514f7a6ae643ac6a1bd2d7d
kawamurashin <kawamura.shin@gmail.com>
2017-12-15 18:55:32 +0900

    first commit

---
+++ Assets/Script/Main.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Script
+{
+	public class Main : MonoBehaviour {
+		// Use this for initialization
+		private void Start ()
+		{
+			GameObject obj;
+			obj = new GameObject("Canvas");
+			var canvas = obj.AddComponent<Canvas>();
+			obj.transform.SetParent(this.transform);
+			
+			obj = new GameObject("Image");
+			var image = obj.AddComponent<Image>();
+			obj.transform.SetParent(canvas.transform);
+			
+			List<Hashtable> list = new List<Hashtable>();
+			Hashtable iTweenHashtable;
+			Hashtable hash;
+			Hashtable hashtable;
+			//serial sample
+			iTweenHashtable = new Hashtable();
+			iTweenHashtable.Add("target", obj);
+			hash = new Hashtable();
+			hash.Add("x",300);
+			hash.Add("time",2);
+			hash.Add("delay",1f);
+			iTweenHashtable.Add("hash",hash);
+			list.Add(iTweenHashtable);
+			//
+			iTweenHashtable = new Hashtable();
+			iTweenHashtable.Add("target", obj);
+			hash = new Hashtable();
+			hash.Add("x",-300);
+			hash.Add("time",5);
+			iTweenHashtable.Add("hash",hash);
+			list.Add(iTweenHashtable);
+			
+			//
+			hashtable = new Hashtable();
+			hashtable.Add("list" , list);
+			hashtable.Add("oncompletetarget", this.gameObject);
+			hashtable.Add("oncomplete" , "OnCompleteSerial");
+			iTweens.Serial(this.gameObject, hashtable);
+
+			//palallele sample
+			list = new List<Hashtable>();
+			//
+			obj = new GameObject("Image");
+			image = obj.AddComponent<Image>();
+			obj.transform.SetParent(canvas.transform);
+			obj.transform.localPosition = new Vector3(0,300);
+			
+			iTweenHashtable = new Hashtable();
+			iTweenHashtable.Add("target", obj);
+			hash = new Hashtable();
+			hash.Add("x",-500);
+			hash.Add("time",1);
+			hash.Add("delay",0f);
+			iTweenHashtable.Add("hash",hash);
+			list.Add(iTweenHashtable);
+			//
+			obj = new GameObject("Image");
+			image = obj.AddComponent<Image>();
+			obj.transform.SetParent(canvas.transform);
+			obj.transform.localPosition = new Vector3(0,100);
+			
+			iTweenHashtable = new Hashtable();
+			iTweenHashtable.Add("target", obj);
+			hash = new Hashtable();
+			hash.Add("x",-400);
+			hash.Add("time",2);
+			hash.Add("delay",0f);
+			iTweenHashtable.Add("hash",hash);
+			list.Add(iTweenHashtable);
+			//
+			obj = new GameObject("Image");
+			image = obj.AddComponent<Image>();
+			obj.transform.SetParent(canvas.transform);
+			obj.transform.localPosition = new Vector3(0,-100);
+			
+			iTweenHashtable = new Hashtable();
+			iTweenHashtable.Add("target", obj);
+			hash = new Hashtable();
+			hash.Add("x",-400);
+			hash.Add("time",10);
+			hash.Add("delay",0f);
+			iTweenHashtable.Add("hash",hash);
+			list.Add(iTweenHashtable);
+			//
+			
+			hashtable = new Hashtable();
+			hashtable.Add("list" , list);
+			hashtable.Add("oncompletetarget", this.gameObject);
+			hashtable.Add("oncomplete" , "OnCompleteParallel");
+			iTweens.Parallel(this.gameObject, hashtable);
+		}
+
+		private void OnCompleteParallel()
+		{
+			Debug.Log("OnCompleteParallel");
+		}
+		private void OnCompleteSerial()
+		{
+			Debug.Log("OnCompleteSerial");
+		}
+	
+		// Update is called once per frame
+		private void Update () {
+		
+		}
+	}
+}
---
+++ Assets/Script/iTweens.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class iTweens : MonoBehaviour
+{
+	private static iTweens _instance;
+	public static void Serial(GameObject target, Hashtable args)
+	{
+		_instance = target.AddComponent<iTweens>();	
+		var hashtable = new Hashtable();
+		hashtable.Add("count",0);
+		hashtable.Add("target", target);
+		hashtable.Add("args", args);
+		SerialTween(hashtable);
+	}
+	
+	public static void Parallel(GameObject target, Hashtable args)
+	{
+		_instance = target.AddComponent<iTweens>();	
+		var hashtable = new Hashtable();
+		hashtable.Add("count",0);
+		hashtable.Add("target", target);
+		hashtable.Add("args", args);
+		ParallelTween(hashtable);
+	}
+
+	private static void SerialTween(Hashtable hashtable)
+	{
+		var target = (GameObject)hashtable["target"];
+		var args = (Hashtable)hashtable["args"];
+		var list = (List<Hashtable>)args["list"];
+		var iTweenHashtable = list[(int) hashtable["count"]];
+		var go = (GameObject) iTweenHashtable["target"];
+		var hash = (Hashtable) iTweenHashtable["hash"];
+		hash.Add ("oncompletetarget", target);
+		hash.Add("oncompleteparams", hashtable);
+		hash.Add ("oncomplete", "SerialTweenComplete");
+		iTween.MoveAdd(go,hash);
+	}
+	
+	private static void ParallelTween(Hashtable hashtable)
+	{
+		var target = (GameObject)hashtable["target"];
+		var args = (Hashtable)hashtable["args"];
+		var list = (List<Hashtable>)args["list"];
+		hashtable["count"] = 0;
+		var n = list.Count;
+		for (var i = 0; i < n; i++)
+		{
+			var iTweenHashtable = list[i];
+			var go = (GameObject) iTweenHashtable["target"];
+			var hash = (Hashtable) iTweenHashtable["hash"];
+			hash.Add ("oncompletetarget", target);
+			hash.Add("oncompleteparams", hashtable);
+			hash.Add ("oncomplete", "ParallelTweenComplete");
+			iTween.MoveAdd(go,hash);
+		}
+	}
+
+	protected void SerialTweenComplete(Hashtable hashtable)
+	{
+		var args = (Hashtable)hashtable["args"];
+		var list = (List<Hashtable>)args["list"];
+		hashtable["count"] = 1 + (int)hashtable["count"];
+		if ((int) hashtable["count"] == list.Count)
+		{
+			AllComplete(hashtable);
+		}
+		else
+		{
+			SerialTween(hashtable);
+		}
+	}
+
+	protected void ParallelTweenComplete(Hashtable hashtable)
+	{
+		var args = (Hashtable)hashtable["args"];
+		var list = (List<Hashtable>)args["list"];
+		hashtable["count"] = 1 + (int)hashtable["count"];
+		if ((int) hashtable["count"] == list.Count)
+		{
+			AllComplete(hashtable);
+		}
+	}
+
+	private void AllComplete(Hashtable hashtable)
+	{
+		Destroy(this);
+		var args = (Hashtable)hashtable["args"];
+		var target = (GameObject) args["oncompletetarget"];
+		var complteFunc = (string) args["oncomplete"];
+		target.SendMessage(complteFunc);
+	}
+}
